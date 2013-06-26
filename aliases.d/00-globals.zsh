61757a0d03755280fbde88ce8156549639296ed5
Jan 'Khardix' Staněk <khardix@gmail.com>
2013-06-26 09:45:19 +0200

    KDE shutdown && logout commands

--- aliases.d/00-globals.zsh
+++ aliases.d/00-globals.zsh
@@ -1,7 +1,25 @@
 # Command aliases
 
-alias poweroff='systemctl poweroff'
 alias vimide='vim +NERDTree +TagbarOpen'
+alias gide='gvim +NERDTree +TagbarOpen'
+
+function logout
+{
+  if [ "$XDG_CURRENT_DESKTOP" = "KDE" ]; then
+    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+  else
+    logout
+  fi
+}
+
+function poweroff
+{
+  if [ "$XDG_CURRENT_DESKTOP" = "KDE" ]; then
+    qdbus org.kde.ksmserver /KSMServer logout 0 2 2
+  else
+    systemctl poweroff
+  fi
+}
 
 # Suffix aliases
 alias -s php='php -f'
