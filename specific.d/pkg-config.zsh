d4a9bf84fcf14b2da4e1d5819e71be1dc5b692a7
Jan "Khardix" Staněk <khardix@gmail.com>
2014-11-30 19:25:26 +0100

    Configuration update, new specifics

---
+++ specific.d/pkg-config.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/zsh
+
+# Add local dirs to search
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
---
+++ specific.d/vimpager.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/zsh
+
+export PAGER=/usr/bin/vimpager
+alias less='vimpager'
