22aa79f200d1b6c4bc606f06d60c7affc61d567b
Jan "Khardix" Staněk <khardix@gmail.com>
2015-07-14 11:04:41 +0200

    Add coloring of man pages
    - Drop old and unused wrapper function aliases.

--- aliases.d/00-globals.zsh
+++ aliases.d/00-globals.zsh
@@ -10,29 +10,6 @@ alias mpv-us='mpv --video-unscaled'
 alias ssc='sudo systemctl'
 alias scu='systemctl --user'
 
-function logout
-{
-  if [ "$XDG_CURRENT_DESKTOP" = "KDE" ]; then
-    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-  else
-    logout
-  fi
-}
-
-function poweroff
-{
-  if [ "$XDG_CURRENT_DESKTOP" = "KDE" ]; then
-    qdbus org.kde.ksmserver /KSMServer logout 0 2 2
-  else
-    systemctl poweroff
-  fi
-}
-
-function subconv
-{
-  iconv -f WINDOWS-1250 -t UTF-8 "$1"|dos2unix
-}
-
 function ggr # goto git root
 {
   local topdir="$(git rev-parse --show-toplevel 2>/dev/null)"
@@ -58,6 +35,19 @@ function update-configs
   git pull --recurse-submodules="$submodules" && git submodule foreach git merge
   bash setup.sh # run dotbot
   echo "Leaving config directory..." && popd &>/dev/null
+}
+
+# Colored man pages
+function man
+{
+  env LESS_TERMCAP_mb=$'\E[01;31m' \
+  LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+  LESS_TERMCAP_me=$'\E[0m' \
+  LESS_TERMCAP_se=$'\E[0m' \
+  LESS_TERMCAP_so=$'\E[38;5;246m' \
+  LESS_TERMCAP_ue=$'\E[0m' \
+  LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+  man "$@"
 }
 
 # Suffix aliases
