fd9930f647e5a6edf44e6c899ab7d455ee4313fe
Jan Staněk <jstanek@redhat.com>
2016-02-08 13:10:57 +0100

    Add dynamic alias to study materials.

--- dirs.d/00-globals.zsh
+++ dirs.d/00-globals.zsh
@@ -1,8 +1,19 @@
 # Global named directories
 
-export CDPATH=.:~
+## Utility functions
 
+function current_semester()
+{
+  setopt local_options null_glob
+  local semesters=($(print -l ~/(#i)(documents)/uni/mgr/sem-?))
+  echo ${semesters[-1]}
+}
+
+# Misc directories
 hash -d pthemes=~zsh/oh-my-zsh/themes
 hash -d config=~/system/dotfiles
 hash -d vim=~config/vim
 hash -d bin=~/.local/bin
+
+# Current semester alias
+hash -d uni="$(current_semester)"
