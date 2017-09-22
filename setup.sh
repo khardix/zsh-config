e30a04805f5b6f870f207521621c761f9b0d85ef
Jan Staněk <jstanek@redhat.com>
2017-09-22 09:31:40 +0200

    Switch to dedicated dotbot configuration for linking

--- setup.sh
+++
@@ -1,5 +0,0 @@
-# if in empty dir, clone self
-test -z "$(ls .)" && git clone --recursive git@github.com:khardix/zsh-config.git .
-
-# link zshrc
-test -f "$HOME/.zshrc" || ln -s "$PWD/zshrc" "$HOME/.zshrc"
