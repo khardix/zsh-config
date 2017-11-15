2e56add50568c7f9d34e91b3e95cbe266165c281
Jan Khardix Staněk <khardix@gmail.com>
2017-11-15 19:22:21 +0100

    Port old plugins to zplug format

--- plugs.zsh
+++ plugs.zsh
@@ -18,5 +18,18 @@ source "$ZPLUG_HOME/init.zsh"
 # => Let zplug manage itself
 zplug "zplug/zplug", hook-build:"zplug --self-manage"
 
+# => Utilize zsh-users basics
+zplug "zsh-users/zsh-completions"       # Omnibus completion definitions
+zplug "zsh-users/zsh-syntax-highlighting", defer:2  # Color the typed command
+
+# => Cherry-pick plugins from oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh     # Aliases for git subcommands
+zplug "plugins/pip", from:oh-my-zsh     # Cached package completion for pip
+zplug "plugins/python", from:oh-my-zsh  # Completion and clean function for python
+zplug "plugins/taskwarrior", from:oh-my-zsh  # Smart task and project completion
+
+# => Manage my own plugins
+zplug "khardix/fedpkg-zsh", if:"[[ -n ${commands[fedpkg]} || -n ${commands[rhpkg]} ]]"
+
 # ===== Finalization =====
 zplug load          # Source all installed plugins and add them to $path
