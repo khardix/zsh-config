47205dc4311c662db49aa69521bac60135043ce3
Jan Khardix Staněk <khardix@gmail.com>
2017-11-19 12:37:37 +0100

    Add dynamic paths for aliases and named dirs
    - Setup named dirs for university files, local binaries and a mount point

---
+++ named-dirs.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+# Extra named directories
+
+# XDG directory shortcuts
+hash -d bin=~/.local/bin
+
+# Udisks2 mount point
+hash -d dmnt=/run/media/"$USER"
+
+# University related projects and documents
+hash -d uniproj="$(print ~/(#i)(projects)/uni/mgr/semestr-?(/OnY1N^M))"
+hash -d unidoc="$(print ~/(#i)(documents)/uni/mgr/semestr-?(/OnY1N^M))"
