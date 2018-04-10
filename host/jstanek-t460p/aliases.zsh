1f7242fdbb7e47f7de2546c04bd751b7ee3a1020
Jan Staněk <jstanek@redhat.com>
2018-04-10 12:35:32 +0200

    Do not pass custom environment to rpmbuild

--- host/jstanek-t460p/aliases.zsh
+++ host/jstanek-t460p/aliases.zsh
@@ -4,3 +4,6 @@
 # Use vim with X support by default
 alias vim='vimx'
 alias view='vimx -R'
+
+# Do not pass build flags to local RPM builds
+alias rpmbuild='env --unset=CFLAGS --unset=CXXFLAGS -- rpmbuild'
